e697a9caddcda49ed54d934c98bda122fe13a4ac
ishluxx <ishimweemmanuel033@gmail.com>
2024-04-15 07:10:15 +0200

    add

--- handler.js
+++
@@ -1,9 +0,0 @@
-// handler.js (serverless function)
-const { createServer, proxy } = require('vercel-micro');
-
-const server = require('./App'); // Import your JSON server code
-
-module.exports = createServer((req, res) => {
-  // Pass the request and response objects to your JSON server
-  server(req, res);
-});
